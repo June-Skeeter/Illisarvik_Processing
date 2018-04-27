ca102d15290a301247a9d579c64d2e19d234e32a
Wes-Skeeter <wesley.skeeter@alumni.ubc.ca>
2018-04-27 11:17:33 +0200

    kitty

--- Footprint_Processing.py
+++ Footprint_Processing.py
@@ -1,7 +1,6 @@
 
 import sys
-import os
-import time
+
 import numpy as np
 import pandas as pd
 import geopandas as gpd
@@ -24,6 +23,21 @@ from Klujn_2015_FootprinModel.calc_footprint_FFP_climatology_SkeeterEdits import
 
 class Calculate(object):
 	"""docstring for Calculate"""
-	def __init__(self, arg):
+	def __init__(self,Data,Domain,XY,nx=1000,dx=1,rs=[50,75,90]):
 		super(Calculate, self).__init__()
-		self.arg = arg
+		print(Data)
+		self.Runs = Data.shape[0]
+		self.Data = Data
+		self.Domain = rasterio.open(Domain,'r')
+		self.Image = self.Domain.read(1)
+		self.Template = self.Image*0
+		self.fp_params={'dx':dx,'nx':nx,'rs':rs}
+		self.Prog = prb.ProgressBar(self.Runs)
+		plt.imshow(self.Image)
+
+		self.run()
+
+	def run(self):
+		for i in range(self.Runs):
+			Name = str(self.Data['datetime'].iloc[i]).replace(' ','_').replace('-','').replace(':','')
+			self.Prog.Update(i)
